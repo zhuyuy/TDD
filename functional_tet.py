a89df7ab5825974fd3cbace86b44b4c4baaa8779
jingyu <jingyuzhao@foxmail.com>
2020-04-14 10:40:05 +0800

    front page Html now generated from a tempplate

--- functional_tet.py
+++ functional_tet.py
@@ -34,10 +34,11 @@ class NewVisitorTesst(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id list_table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows)
+            any(row.text == '1:Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         #there is still a tet box inviting her to add another item. She enters"use peacock feathers to make a fly'(Edith is very methodical)
