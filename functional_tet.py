edf2e1bfafeb504ad2ac941f1680865a7eef75d9
jingyu <jingyuzhao@foxmail.com>
2020-04-21 11:11:26 +0800

    refactoring

--- functional_tet.py
+++ functional_tet.py
@@ -10,36 +10,10 @@ class NewVisitorTesst(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    """ def test_can_start_a_list_and_retrieve_it_later(self):
-        #Edith has heard about a cool new online to-do app She goes to chceck out its homepage
-        self.browser.get('http://localhost:8000')
-
-        #She noticecs the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-
-        #she is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-
-        #she types 'buy peacock feathers' into a text box (Edith's hobby is tying fly-finishing lures)
-        inputbox.send_keys('Buy peacock feathers')
-
-        #when she hits enter, the page updates,and now the page lists
-        #'1:buy peacock feathers' as an item in a to-do list
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
+    def check_for_row_in_list_table(self,row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn( '1: Buy peacock feathers', [row.text for row in rows])
-
-        #there is still a tet box inviting her to add another item. She enters"use peacock feathers to make a fly'(Edith is very methodical)
-        self.fail('finish the test!') """
+        self.assertIn( row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Edith has heard about a cool new online to-do app She goes to chceck out its homepage
@@ -58,11 +32,8 @@ class NewVisitorTesst(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn( '1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Usepeacock featheres to make a fly',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feahters')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         #there is still a tet box inviting her to add another item. She enters"use peacock feathers to make a fly'(Edith is very methodical)
         self.fail('finish the test!')
