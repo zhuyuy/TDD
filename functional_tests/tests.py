fc17944f52edef8af6f2dfebd128fa9c9e75295e
jingyu <jingyuzhao@foxmail.com>
2020-06-25 17:43:32 +0800

    ft第一次layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -56,11 +56,12 @@ class NewVisitorTest(LiveServerTestCase):
         #there is still a tet box inviting her to add another item. She enters"use peacock feathers to make a fly'(Edith is very methodical)
         self.fail('finish the test!')
 
-    
+    '''
     def test_can_start_a_list_for_one_user(self):
         #Edith听说了这个tdd的app
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        '''
     
 
     def test_mutiple_users_can_start_lists_at_different_urls(self):
@@ -99,6 +100,19 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+    def test_layout_and_styling(self):
+        #Edith 回到主页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        #他注意到输入框在中央
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=70
+        )
+        
 
 
 
