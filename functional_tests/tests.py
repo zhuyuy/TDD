cff5e58a37960f54478cfe86dd06273b81671102
jingyu <jingyuzhao@foxmail.com>
2020-06-25 19:39:37 +0800

    able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,11 +3,16 @@ from selenium import webdriver#(1)
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+import os
 
 MAX_WAIT = 10
 
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
+        self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
